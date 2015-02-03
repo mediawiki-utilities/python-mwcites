fba97d5ce99e092ce15ac28581babeca7d64858d
Aaron Halfaker <aaron.halfaker@gmail.com>
2015-02-03 14:53:38 +0000

    Changes the meaning if rev_id and timestamp to be when the cite was added.

--- pmids/extract_pmids.py
+++ pmids/extract_pmids.py
@@ -14,15 +14,17 @@ import re
 import docopt
 from mw import xml_dump
 
-PMID_RE = re.compile(r"\b(pmid|pmc) *= *([0-9]+)\b", re.I)
+PMID_RE = re.compile(r"\b(pmid|pmc) *= *(pmc)?([0-9]+)\b", re.I)
 
 # See: https://en.wikipedia.org/w/index.php?
 #      title=Wikipedia%3AVillage_pump_%28technical%29&
 #      diff=630990203&oldid=630985731
-PMURL_RE = re.compile(r"http://www\.ncbi\.nlm\.nih\.gov" +
+PMURL_RE = re.compile(r"//www\.ncbi\.nlm\.nih\.gov" +
                       r"/pubmed/([0-9]+)", re.I)
-PMCURL_RE = re.compile(r"http://www\.ncbi\.nlm\.nih\.gov" +
+PMCURL_RE = re.compile(r"//www\.ncbi\.nlm\.nih\.gov" +
                        r"/pmc/articles/PMC([0-9]+)", re.I)
+
+
 
 def main():
     args = docopt.docopt(__doc__)
@@ -30,12 +32,15 @@ def main():
     def process_dump(dump, path):
         
         for page in dump:
+            if page.namespace != 0: continue
             
+            id_appearance = {}
+            ids = set()
             for revision in page:
                 ids = set()
                 
                 for match in PMID_RE.finditer(revision.text or ""):
-                    ids.add((match.group(1), int(match.group(2))))
+                    ids.add((match.group(1).lower(), int(match.group(3))))
                 
                 for match in PMURL_RE.finditer(revision.text or ""):
                     ids.add(("pmid", int(match.group(1))))
@@ -43,19 +48,25 @@ def main():
                 for match in PMCURL_RE.finditer(revision.text or ""):
                     ids.add(("pmc", int(match.group(1))))
                 
-                for type, id in ids:
-                    yield (page.id, page.namespace, page.title, revision.id,
-                           type, id)
+                for id in ids:
+                    if id not in id_appearance:
+                       id_appearance[id] = (revision.id, revision.timestamp)
+                
+            for id in ids: #For the ids in the last version of the page
+                appearance_id, appearance_timestamp = id_appearance[id]
+                yield (page.id, page.title, appearance_id, appearance_timestamp, id[0], id[1])
                 
             
         
     
-    print("page_id\tpage_namespace\tpage_title\trev_id\ttype\tid")
+    print("page_id\tpage_title\trev_id\ttimestamp\ttype\tid")
     
     id_metas = xml_dump.map(args['<dump_path>'], process_dump)
-    for page_id, namespace, title, rev_id, type, id in id_metas:
+    for page_id, title, rev_id, timestamp, type, id in id_metas:
         
         print("{0}\t{1}\t{2}\t{3}\t{4}\t{5}"\
-              .format(page_id, namespace,
+              .format(page_id,
                       title.replace("\t", "\\t").replace("\n", "\\n"),
-                      rev_id, type, id))
+                      rev_id, 
+                      timestamp.long_format(),
+                      type, id))
