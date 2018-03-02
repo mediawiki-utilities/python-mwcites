48feb82d80fbf7c38e57d40b5aff58be41f15dc1
Baha <bmansurov@wikimedia.org>
2018-03-02 16:31:23 +0500

    Correctly parse ISBNs with space
    Sometimes, e.g. [1], ISBN numbers are typed erroneously as
    "isbn=2 906700-09-6" (notice the space after 2). This patch recognizes
    such ISBNs as valid.
    
    [1] https://fr.wikipedia.org/w/index.php?diff=prev&oldid=83778617

--- mwcites/extractors/isbn.py
+++ mwcites/extractors/isbn.py
@@ -1,8 +1,13 @@
 import re
 from ..identifier import Identifier
 
-ISBN_RE = re.compile('isbn\s?=?\s?([0-9\-Xx]+)', re.I)
+# Also correctly parses malformed inputs such as below:
+# isbn=2 906700-09-6 (malformed input — notice the space)
+ISBN_RE = re.compile('isbn\s?=?\s?(([0-9]+\s)?([0-9\-Xx]+))', re.I)
 
 def extract(text):
     for match in ISBN_RE.finditer(text):
-        yield Identifier('isbn', match.group(1).replace('-', ''))
+        yield Identifier(
+            'isbn',
+            match.group(1).replace('-', '').replace(' ', '')
+        )
