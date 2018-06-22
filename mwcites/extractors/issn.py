c9ea4f83e14bdeb9e95d0efc136810a433dbb52d
Xarvalus <GromadzkiMarcin@gmail.com>
2018-06-22 23:03:01 +0200

    Stripping space characters

--- mwcites/extractors/issn.py
+++ mwcites/extractors/issn.py
@@ -5,4 +5,7 @@ ISSN_RE = re.compile('issn\s?=?\s?([0-9]{4}\-[0-9]{3}([0-9]|X))', re.I)
 
 def extract(text):
     for match in ISSN_RE.finditer(text):
-        yield Identifier('issn', match.group(1).replace('-', ''))
+        yield Identifier(
+            'issn',
+            match.group(1).replace('-', '').replace(' ', '').strip()
+        )
